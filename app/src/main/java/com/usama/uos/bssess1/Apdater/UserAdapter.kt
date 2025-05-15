dc902f4497ea6776e91e0aceaa3c5da1ee7abaa0
mhmd.usama.hafeez <mhmd.usama.hafeez@gmail.com>
2025-05-15 15:20:24 +0500

    Realtime DB data Fetch

---
+++ app/src/main/java/com/usama/uos/bssess1/Apdater/UserAdapter.kt
@@ -0,0 +1,38 @@
+package com.usama.uos.bssess1.Apdater
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.usama.uos.bssess1.Models.UserModel
+import com.usama.uos.bssess1.R
+
+class UserAdapter(private var userArrayList: ArrayList<UserModel>, context: Context) :
+   RecyclerView.Adapter<UserAdapter.MyUserViewHolder>() {
+   private var inflator: LayoutInflater = LayoutInflater.from(context)
+
+
+   inner class MyUserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+      val txtUserName: TextView = itemView.findViewById(R.id.txtUserName)
+      val txtUserEmail: TextView = itemView.findViewById(R.id.txtUserEmail)
+   }
+
+   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyUserViewHolder {
+      val view = inflator.inflate(R.layout.list_items_users, null)
+      return MyUserViewHolder(view)
+   }
+
+   override fun getItemCount(): Int {
+      return userArrayList.size
+   }
+
+   override fun onBindViewHolder(holder: MyUserViewHolder, position: Int) {
+      val model = userArrayList[position]
+      holder.txtUserEmail.text = model.userEmailAddress
+      holder.txtUserName.text = model.firstName
+
+   }
+
+}
--- app/src/main/java/com/usama/uos/bssess1/Fragments/UserProfileFragment.kt
+++ app/src/main/java/com/usama/uos/bssess1/Fragments/UserProfileFragment.kt
@@ -1,23 +1,108 @@
 package com.usama.uos.bssess1.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.usama.uos.bssess1.Apdater.UserAdapter
+import com.usama.uos.bssess1.Models.UserModel
 import com.usama.uos.bssess1.R
 
 
 class UserProfileFragment : Fragment() {
 
+   lateinit var rvUsers:RecyclerView
+   lateinit var pbUserProfile:ProgressBar
+   lateinit var noDataExit:TextView
+   lateinit var userDatabase: FirebaseDatabase
+   lateinit var firebaseRef: DatabaseReference
+   lateinit var arrayListUsers: ArrayList<UserModel>
+   lateinit var userAdapter: UserAdapter
 
    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
       val view = inflater.inflate(R.layout.fragment_user_profile, container, false)
 
+      rvUsers = view.findViewById(R.id.rvUsers)
+      pbUserProfile = view.findViewById(R.id.pbUserProfile)
+      noDataExit = view.findViewById(R.id.noDataExit)
 
-
-
+      getUsers()
       return view
+
+   }
+
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+      super.onViewCreated(view, savedInstanceState)
+   }
+
+
+
+
+
+   private fun getUsers(){
+      pbUserProfile.visibility = ProgressBar.VISIBLE
+
+      userDatabase = FirebaseDatabase.getInstance()
+      firebaseRef = userDatabase.getReference("Users")
+      arrayListUsers = ArrayList()
+
+      firebaseRef.addListenerForSingleValueEvent(object:ValueEventListener{
+         override fun onDataChange(snapshot: DataSnapshot) {
+            try {
+
+               for (eachUser in snapshot.children){
+                  val key = eachUser.key.toString()
+
+                  val userModel: UserModel? = eachUser.getValue(UserModel::class.java)
+                  userModel?.userID = key
+
+                  arrayListUsers.add(userModel!!)
+
+               }
+               if(arrayListUsers == null || arrayListUsers.size == 0){
+                  noDataExit.visibility = TextView.VISIBLE
+                  pbUserProfile.visibility = ProgressBar.GONE
+
+               }else{
+                  userAdapter = UserAdapter(arrayListUsers , requireActivity())
+                  rvUsers.adapter = userAdapter
+                  pbUserProfile.visibility = ProgressBar.GONE
+
+               }
+
+
+
+            }catch (e:Exception){
+               Log.e("DB Error" , "Failed: ${e}")
+
+            }
+
+
+         }
+
+         override fun onCancelled(error: DatabaseError) {
+             Toast.makeText(requireActivity(), "Failed: ${error.message}" , Toast.LENGTH_SHORT).show()
+            Log.e("DB Error" , "Failed: ${error.message}")
+         }
+
+      })
+
+      /*firebaseRef.addValueEventListener(object :ValueEventListener{
+
+      })*/
+
+
 
    }
 
